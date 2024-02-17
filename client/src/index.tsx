91c34cedcc019f2ee826b20588e947658c5efb97
Vladislav Kireev <kireew.vladislaw@gmail.com>
2024-02-17 20:32:24 +0300

    feat: header

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter, Router } from "react-router-dom";
+import history from "./utilits/history";
 import "./assets/scss/styles.scss";
-import App from "./App";
+import "./index.ts";
+import App from "./components/app/app";
+import store from "./services/index";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store()}>
+      <BrowserRouter basename="/">
+        {/* <Router history={history}> */}
+        <App />
+        {/* </Router> */}
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
